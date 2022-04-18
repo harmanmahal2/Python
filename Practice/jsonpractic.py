ee05670776c9aac2ecf6522202825c9344dc513f
Harman Mahal <harman.mahal@techota.com>
2022-04-18 08:54:37 -0500

    practice at work2

--- Practice/jsonpractic.py
+++ Practice/jsonpractic.py
@@ -2,13 +2,29 @@ import json
 
 #numbers = [1,2,3,4,5,6,6]
 
-file = 'files/numbers.json'
+#file = 'files/numbers.json'
 
 #with open(file, 'w') as f:
   #  json.dump(numbers, f)
 
 
-with open(file) as f:
-    number = json.load(f)
+#with open(file) as f:
+    #number = json.load(f)
 
-print(number)
+#print(number)
+
+
+
+## 10-11
+
+path = 'files/fav_number.json'
+
+if path:
+    with open(path) as j:
+        fav = json.load(j)
+        print(fav)
+else:
+    fav_number = input("What is yur fav number")
+    with open(path, 'w') as f:
+        json.dump(fav_number, f)
+
