ad5655b7710d612e5d57ff0a202eae95601c80f2
Harman Mahal <harman.mahal@techota.com>
2022-04-20 13:43:44 -0500

    Rammie Game

--- RammieGame/RammieGame.py
+++ RammieGame/RammieGame.py
@@ -1,7 +1,10 @@
 import sys
 import pygame
+import self as self
+
 from Settings import Settings
 from icon_rammie import Rammie
+
 
 class RammieGame:
     """overall class to manage assets and behaviour"""
@@ -14,26 +17,57 @@ class RammieGame:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Ramneet's Game")
         self.rammie = Rammie(self)
-        self.bg_color = (230,230,230)
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """start main loop for the game"""
         while True:
-            #watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.rammie.update()
+            self._update_screen()
 
+            # watch for keyboard and mouse events
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.rammie.blitme()
+    def _check_events(self):
+        # respond to keypress
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+               self._check_keyup_events(event)
 
-            # Make the most recent drawn screen visible
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.rammie.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.rammie.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.rammie.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.rammie.moving_down = True
 
-            pygame.display.flip()
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.rammie.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.rammie.moving_left = False
+        if event.key == pygame.K_UP:
+            self.rammie.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.rammie.moving_down = False
+
+    def _update_screen(self):
+        """update images on the screen, and flip to new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.rammie.blitme()
+
+        # Make the most recent drawn screen visible
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
-    ai = RammieGame()
-    ai.run_game()
+    rg = RammieGame()
+    rg.run_game()
--- RammieGame/icon_rammie.py
+++ RammieGame/icon_rammie.py
@@ -3,14 +3,43 @@ import pygame
 
 class Rammie:
 
-    def __init__(self, ai_game):
-        self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
+    def __init__(self, rammie_game):
+        self.screen = rammie_game.screen
+        self.settings = rammie_game.settings
+        self.screen_rect = rammie_game.screen.get_rect()
 
         self.image = pygame.image.load('images/rammie.png')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        #movement flag
+
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+
+
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.rammie_speed
+
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.rammie_speed
+
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.rammie_speed
+
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.y -= self.settings.rammie_speed
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
