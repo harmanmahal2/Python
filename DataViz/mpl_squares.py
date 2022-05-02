69fdcba1767ce1f0dbda3f72afebcd652cd12ae6
Harman Mahal <harman.mahal@techota.com>
2022-05-02 12:35:04 -0500

    graphs

--- DataViz/mpl_squares.py
+++ DataViz/mpl_squares.py
@@ -1,8 +1,25 @@
 import matplotlib.pyplot as plt
+plt.style.available
 
-squares = [1,4,9,16,25]
+input_values = [1,5,10,15,20,25,30]
+squares = []
 
+for each in input_values:
+    sq = each * each
+    squares.append(sq)
+
+plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values,squares, linewidth=3)
 
-plt.show
+#set chart title and label axes
+
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+#set size of tick labels
+ax.tick_params(axis='both', labelsize=14)
+
+
+plt.show()
---
+++ DataViz/scatter_squares.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn')
+fix, ax = plt.subplots()
+ax.scatter(2,4, s=200)
+
+plt.show()
