d98f03dd8d653286fa1b22e953742ad16c5d0d13
Harman Mahal <harman.mahal@techota.com>
2022-05-18 13:17:49 -0500

    graphs

--- DataViz/rw_visual.py
+++ DataViz/rw_visual.py
@@ -3,12 +3,16 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(100000)
     rw.fill_walk()
 
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c= point_numbers, cmap= plt.cm.Blues, edgecolors= 'none', s=1)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("make another walk? (y/n)")
