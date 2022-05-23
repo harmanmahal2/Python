fcf53a040c853d4008662d6bc922d86e1ba6e720
Harman Mahal <harman.mahal@techota.com>
2022-05-23 13:29:29 -0500

    football discussion

--- Django/football/base/urls.py
+++ Django/football/base/urls.py
@@ -3,5 +3,5 @@ from. import views
 
 urlpatterns = [
     path('', views.home, name="Home"),
-    path('room/', views.room, name="Room")
+    path('room/<str:pk>/', views.room, name="Room")
 ]
--- Django/football/base/views.py
+++ Django/football/base/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
 
-def room(request):
-    return render(request, 'room.html')
+rooms = [
+    {'id':1, 'name':'Lets make a soccer discussion panel!'},
+    {'id':2, 'name':'Lets design'},
+    {'id':3, 'name':'Test Test Test '},
+
+]
+
+
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
