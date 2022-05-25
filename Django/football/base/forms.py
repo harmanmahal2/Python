6ad8f0c2381406477131b70a7edfd7c77f721e62
Harman Mahal <harman.mahal@techota.com>
2022-05-25 14:48:56 -0500

    football discussion

---
+++ Django/football/base/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from .models import Room
+
+
+##function based views
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
--- Django/football/base/views.py
+++ Django/football/base/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Create your views here.
 from .models import Room
+from .forms import RoomForm
+
 
 #rooms = [
    # {'id':1, 'name':'Lets make a soccer discussion panel!'},
@@ -20,3 +22,35 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room) #to find which room to edit
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room) ## to tell which room to edit
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
