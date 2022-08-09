ecf1afc18f37908ac8f07aa671d04327b819b2aa
Harman Mahal <harman.mahal@techota.com>
2022-08-09 17:13:33 -0500

    matches complete with input validation

--- your_stats/match_results/urls.py
+++ your_stats/match_results/urls.py
@@ -5,12 +5,15 @@ app_name = 'match_results'
 urlpatterns = [
     path('', views.index, name='index'),
     path('matches/', views.matches , name='matches'),
+    path('search_matches/', views.search_matches, name='search_matches'),
     path('matches/<int:match_id>/', views.match, name='match'),
     path('leagues/', views.leagues , name='leagues'),
     path('league/<int:league_id>/', views.league , name='league'),
     path('players/', views.players, name='players'),
+    path('search_players/', views.search_players, name='search_players'),
     path('players/<int:player_id>/', views.player, name='player'),
     path('teams/', views.teams, name='teams'),
+    path('search_teams/', views.search_teams, name='search_teams'),
     path('teams/<int:team_id>/', views.team, name='team'),
 
 ]
