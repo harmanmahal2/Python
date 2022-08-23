8f712f433f77fb8e253e560df83d07c129cf3a27
Harman Mahal <harman.mahal@techota.com>
2022-08-23 16:08:52 -0500

    add teams complete

--- your_stats/match_results/forms.py
+++ your_stats/match_results/forms.py
@@ -1,3 +1,25 @@
-from django import forms
+from django.forms import ModelForm
 from .models import *
 
+class PlayerForm(ModelForm):
+
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+
+class CreateLeagueForm(ModelForm):
+
+    class Meta:
+        model = League
+        fields = '__all__'
+
+
+class CreateTeamForm(ModelForm):
+
+    class Meta:
+        model = Team
+        fields = '__all__'
+        labels = {
+            "league": "League/Cup"
+        }
--- your_stats/match_results/urls.py
+++ your_stats/match_results/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('search_matches/', views.search_matches, name='search_matches'),
     path('matches/<int:match_id>/', views.match, name='match'),
     path('leagues/', views.leagues , name='leagues'),
+    path('search_leagues/', views.search_leagues, name='search_leagues'),
     path('league/<int:league_id>/', views.league , name='league'),
     path('players/', views.players, name='players'),
     path('search_players/', views.search_players, name='search_players'),
@@ -15,5 +16,7 @@ urlpatterns = [
     path('teams/', views.teams, name='teams'),
     path('search_teams/', views.search_teams, name='search_teams'),
     path('teams/<int:team_id>/', views.team, name='team'),
+    path('create_tournament/', views.create_tournament, name='create_tournament'),
+    path('create_team_ct/', views.create_team_ct, name='create_team_ct')
 
 ]
